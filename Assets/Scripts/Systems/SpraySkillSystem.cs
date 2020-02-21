2737dd959adec3ad55db0af8536a13423458c769
HeBomou <paakmau@outlook.com>
2020-02-21 19:19:03 +0800

    Fix spray skill bug temporarily

--- Assets/Scripts/Systems/SpraySkillSystem.cs
+++ Assets/Scripts/Systems/SpraySkillSystem.cs
@@ -23,7 +23,7 @@ namespace JustFight {
                             skillCmpt.skillShootRecoveryleftTime += skillCmpt.skillShootRecoveryTime;
                             var bulletEntity = ecb.Instantiate (entityInQueryIndex, bulletCmpt.bulletPrefab);
                             ecb.SetComponent (entityInQueryIndex, bulletEntity, new Rotation { Value = quaternion.LookRotation (shootInputCmpt.dir, math.up ()) });
-                            ecb.SetComponent (entityInQueryIndex, bulletEntity, new Translation { Value = localToWorldCmpt.Position + shootInputCmpt.dir * 1f });
+                            ecb.SetComponent (entityInQueryIndex, bulletEntity, new Translation { Value = localToWorldCmpt.Position + shootInputCmpt.dir * 1.6f });
                             ecb.SetComponent (entityInQueryIndex, bulletEntity, new PhysicsVelocity { Linear = shootInputCmpt.dir * bulletCmpt.bulletShootSpeed * 0.7f });
                             ecb.SetComponent (entityInQueryIndex, bulletEntity, new BulletTeam { id = teamCmpt.id });
                         }
