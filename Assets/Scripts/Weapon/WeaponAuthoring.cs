7c8a3b2c06c302b33a4f93893b2ca81238ab7f1b
HeBomou <paakmau@outlook.com>
2020-02-26 19:50:59 +0800

    Fix weapon bug

--- Assets/Scripts/Weapon/WeaponAuthoring.cs
+++ Assets/Scripts/Weapon/WeaponAuthoring.cs
@@ -8,7 +8,7 @@ namespace JustFight.Weapon {
     class WeaponAuthoring : MonoBehaviour, IConvertGameObjectToEntity {
         public float shootRecoveryTime = 0.4f;
         public void Convert (Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
-            dstManager.AddComponentData (entity, new WeaponState { recoveryTime = shootRecoveryTime });
+            dstManager.AddComponentData (entity, new WeaponState { recoveryLeftTime = shootRecoveryTime, recoveryTime = shootRecoveryTime });
         }
     }
 }
